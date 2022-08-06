4c51bc01d13184dc18f920f2a8100a1a67c42f56
Alyssa Jagonob <alysjag@gmail.com>
2022-08-06 16:52:46 +0800

    updated exer3

--- my-app/src/pages/Exercise3.js
+++ my-app/src/pages/Exercise3.js
@@ -1,18 +1,27 @@
-import React, { useEffect, useState } from "react";
-
-
+import React, { useState } from "react";
 
 const Exercise3 = () => {
-    const [count, setCount] = useState(0);
-    useEffect(() => {
-        setTimeout(() => {
-          setCount((count) => (count < 1 ? count + 1 : 0));
-        }, 1000);
-      });
 
-    return 
-    <>
-    <button onClick={() => { alert('Clicked!'); }}>Click Me</button></>
-  };
-  
-  export default Exercise3;
+ 
+
+        const [count, setCount] = useState(0);
+
+
+        const incrementCount = () => {
+
+            setCount(count + 1);
+        };
+        return (<div className="text-center mt-5">
+            <>
+            <p>Button has been clicked: {count} </p>
+            <button onClick={()=> setCount (count+1)}>Click Me</button>
+            </>
+        </div>
+        );
+
+
+   
+
+
+};
+export default Exercise3;
