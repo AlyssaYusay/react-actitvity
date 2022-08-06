577941b20890101976534914de115cbecbca4120
cyses <106429174+AlyssaYusay@users.noreply.github.com>
2022-08-06 17:12:34 +0800

    Update Exercise4.js
    updated exercise 4

--- my-app/src/pages/Exercise4.js
+++ my-app/src/pages/Exercise4.js
@@ -1,9 +1,12 @@
 const Exercise4 = () => {
-    const Animals = ['dog', 'cat', 'chicken', 'cow', 'sheep', 'horse']
-    return 
-    <ul>
-    {Animals.map}
-    </ul>
-  };
-  
-  export default Exercise4;
+
+    const Animals = ['dog', 'cat', 'chicken', 'cow', 'sheep', 'horse'];
+    return (
+        <div >
+            {Animals.map(items =>
+                <ul><li>{items}</li></ul>
+            )}
+        </div>
+    );
+};
+export default Exercise4;
